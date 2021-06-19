f19727a9225d9f447dbb3c662d829eb2e0de222e
Hicka <hello@hicka.dev>
2021-06-19 21:50:03 +0200

    fix(readFile): stop execution in case of error

--- src/functions/readFile.ts
+++ src/functions/readFile.ts
@@ -9,7 +9,7 @@ export default async function readFile(path: PathLike): Promise<string[]> {
 	return new Promise((resolve, reject) => {
 		rf(path, { encoding: 'utf-8' }, (err, data) => {
 			if (err)
-				reject(err);
+				return reject(err);
 			const arr: string[] = data
 				.split('\n') // Splits the file into an array of lines
 				.map(line => line.replace(/\r$/, '')); // Clears \r in case of CRLF end of line sequence
