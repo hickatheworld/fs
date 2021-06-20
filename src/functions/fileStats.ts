a84af5ae27b7041aceffe7143fc1b718ecb7d643
Hicka <hello@hicka.dev>
2021-06-20 21:39:54 +0200

    fix(fileStats): fix non-existing file error
    see 9e1118fb

--- src/functions/fileStats.ts
+++ src/functions/fileStats.ts
@@ -7,9 +7,11 @@ import readFile from './readFile';
  * @param path Path to the file to get stats of
  * @returns Metrics of the file
  */
-export default async function fileStats(path: PathLike): Promise<FileStats> {
+export default async function fileStats(path: PathLike): Promise<FileStats | undefined> {
 	const stats = {} as FileStats;
 	const file = await readFile(path);
+	if (!file)
+		return undefined;
 	stats.characters = file.map(line => line.length).reduce((prev, current) => prev + current);
 	stats.emptyLines = file.filter(line => line === '').length;
 	stats.lines = file.length;
