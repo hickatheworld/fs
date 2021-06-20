26e58c67051f49343c4418702f9f60d899d52c3f
Hicka <hello@hicka.dev>
2021-06-20 21:38:47 +0200

    feat(readJSON): `undefined` instead of `ENOENT`
    see 9e1118fb

--- src/functions/readJSON.ts
+++ src/functions/readJSON.ts
@@ -3,13 +3,16 @@ import { PathLike, readFile } from 'fs';
 /**
  * Reads a file and parses its content to a JSON object
  * @param path Path to the file to read
- * @returns The file content parsed to a JSON object
+ * @returns The file content parsed to a JSON object or undefined if the file doesn't exist
  */
-export default function readJSON(path: PathLike): Promise<Record<string, unknown>> {
+export default function readJSON(path: PathLike): Promise<Record<string, unknown> | undefined> {
 	return new Promise((resolve, reject) => {
 		readFile(path, { encoding: 'utf-8' }, (err, data) => {
-			if (err)
-				reject(err);
+			if (err) {
+				if (err.code === 'ENOENT')
+					return undefined;
+				return reject(err);
+			}
 			const parsed = JSON.parse(data);
 			resolve(parsed);
 		});
