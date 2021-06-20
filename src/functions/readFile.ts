9e1118fbf3d1a1ec4da8a328ff712ebd807bfa02
Hicka <hello@hicka.dev>
2021-06-20 20:11:29 +0200

    feat(readFile): `undefined` in case of `ENOENT`
    `readFile` will no longer throw an error in case of fs `ENOENT` error

--- src/functions/readFile.ts
+++ src/functions/readFile.ts
@@ -3,13 +3,16 @@ import { PathLike, readFile as rf } from 'fs';
 /**
  * Reads a file
  * @param path Path to the file to read.
- * @returns The file's lines splitted in an array
+ * @returns The file's lines splitted in an array or undefined if the file doesn't exist
  */
-export default async function readFile(path: PathLike): Promise<string[]> {
+export default async function readFile(path: PathLike): Promise<string[] | undefined> {
 	return new Promise((resolve, reject) => {
 		rf(path, { encoding: 'utf-8' }, (err, data) => {
-			if (err)
+			if (err) {
+				if (err.code === 'ENOENT')
+					return resolve(undefined);
 				return reject(err);
+			}
 			const arr: string[] = data
 				.split('\n') // Splits the file into an array of lines
 				.map(line => line.replace(/\r$/, '')); // Clears \r in case of CRLF end of line sequence
